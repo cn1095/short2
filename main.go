51fd7599611655a590a528e78ecf34df7eeba83d
cn1095 <1095053105@qq.com>
2025-12-20 18:01:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ type Storage interface {
     ExistsURL(shortCode string) (bool, error)  
     ListAllURLs() ([]string, error)  
     SaveStats(data []byte) error  
-    GetStats() ([]byte, error  
+    GetSGetStats() ([]byte, error)  
 }  
   
 // Global storage instance  
